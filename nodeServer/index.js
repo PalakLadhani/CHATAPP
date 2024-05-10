fb3005a259404d8b23be2b1847b486fc90b7e482
PalakLadhani <pladhani3@gmail.com>
2024-05-10 16:51:53 +0530

    node server

---
+++ nodeServer/index.js
@@ -0,0 +1,19 @@
+//Node Server which will handle sockert.io connection
+const io = require("socket.io")(8000);
+
+const users = {};
+
+io.on("connection", (socket) => {
+  //(socket.io instance)
+  socket.on("new-user-joined", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  }); //particular connection
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+});
