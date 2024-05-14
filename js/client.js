0ceed679f75edfea2c426ca92885dafc85d26924
PalakLadhani <pladhani3@gmail.com>
2024-05-14 23:32:27 +0530

    updated code

--- js/client.js
+++ js/client.js
@@ -4,7 +4,7 @@ const form = document.getElementById("send-container");
 const messageInput = document.getElementById("messageInp");
 const messageContainer = document.querySelector(".container");
 
-const append = (message, position) => {
+const appendMessage = (message, position) => {
   const messageElement = document.createElement("div");
   messageElement.innerText = message;
   messageElement.classList.add("message");
@@ -12,9 +12,21 @@ const append = (message, position) => {
   messageContainer.appendChild(messageElement);
 };
 
+const appendUserJoinedMessage = (name) => {
+  appendMessage(`${name} joined the chat`, "left");
+};
+
 const name = prompt("Enter your name to join");
 socket.emit("new-user-joined", name);
 
 socket.on("user-joined", (name) => {
-  append(`{$name} joined the chat`, "right");
+  appendUserJoinedMessage(name);
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  appendMessage(`You: ${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = "";
 });
