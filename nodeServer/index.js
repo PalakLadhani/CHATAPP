ba2067bfc9d85f25f597f07eb85f1c7f63184f12
PalakLadhani <pladhani3@gmail.com>
2024-05-14 23:31:41 +0530

    updated code

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,15 +1,15 @@
-//Node Server which will handle sockert.io connection
 const io = require("socket.io")(8000);
 
 const users = {};
 
 io.on("connection", (socket) => {
-  //(socket.io instance)
+  console.log("New connection", socket.id);
+
   socket.on("new-user-joined", (name) => {
-    console.log("New user", name);
+    console.log("New user joined:", name);
     users[socket.id] = name;
-    socket.broadcast.emit("user-joined", name);
-  }); //particular connection
+    socket.broadcast.emit("user-joined", name); // Broadcast to all clients except the sender
+  });
 
   socket.on("send", (message) => {
     socket.broadcast.emit("receive", {
